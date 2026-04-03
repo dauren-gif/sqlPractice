db2638c32034625d311839abe99ae5dd8c2f423a
tdt <dauren.tenes@icloud.com>
2026-04-03 17:24:34 +0500

    hard BETTER

--- main.go
+++ main.go
@@ -5,7 +5,8 @@ import (
 	"fmt"
 	simpleconnection "sqlLesson/featurePostgres/simpleConnection"
 	simplesql "sqlLesson/featurePostgres/simpleSql"
-) 
+	"time"
+)
 
 func main() {
 	ctx := context.Background()
@@ -19,9 +20,24 @@ func main() {
 		panic(err)
 	}
 
-	if err := simplesql.InsertRow(ctx, conn); err != nil {
+	if err := simplesql.InsertRow(
+		ctx,
+		conn,
+		"wwwww",
+		"wwwwwwwwww",
+		false,
+		time.Now(),
+	); err != nil {
 		panic(err)
 	}
-	
+
+	if err := simplesql.UpdateRow(ctx, conn); err != nil {
+		panic(err)
+	}
+
+	if err := simplesql.DeleteRow(ctx, conn); err != nil {
+		panic(err)
+	}
+
 	fmt.Println("succeed")
 }
