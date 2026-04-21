6e900e15a009d744486fa5518beba8e82509030b
tdt <dauren.tenes@icloud.com>
2026-04-21 17:27:29 +0500

    TODO: add httpR

--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"fmt"
-	httpserver "sqlLesson/http_server"
+	"context"
+	simpleconnection "sqlLesson/featurePostgres/simpleConnection"
+	simplesql "sqlLesson/featurePostgres/simpleSql"
 )
 
 func main() {
@@ -11,51 +12,38 @@ func main() {
 	// 	panic(err)
 	// }
 
-	fmt.Println("сервер запущен")
+	// fmt.Println("сервер запущен")
 
-	if err := httpserver.StartHttpServer(); err != nil {
-		fmt.Println("ошибка запуска сервера")
+	// if err := httpserver.StartHttpServer(); err != nil {
+	// 	fmt.Println("ошибка запуска сервера")
+	// }
+
+	ctx := context.Background()
+
+	conn, err := simpleconnection.CreateConnect(ctx)
+	if err != nil {
+		panic(err)
 	}
 
-	// ctx := context.Background()
+	if err := simplesql.CreateTable(ctx, conn); err != nil {
+		panic(err)
+	}
 
-	// conn, err := simpleconnection.CreateConnect(ctx)
-	// if err != nil {
-	// 	panic(err)
-	// }
+	tasks, err := simplesql.SelectTable(ctx, conn)
+	if err != nil {
+		panic(err)
+	}
 
-	// if err := simplesql.CreateTable(ctx, conn); err != nil {
-	// 	panic(err)
-	// }
-
-	// if err := simplesql.InsertRow(
-	// 	ctx,
-	// 	conn,
-	// 	"wwwww",
-	// 	"wwwwwwwwww",
-	// 	false,
-	// 	time.Now(),
-	// ); err != nil {
-	// 	panic(err)
-	// }
-
-	// fmt.Println("succeed")
-
-	// tasks, err := simplesql.SelectTable(ctx, conn)
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	// for _, task := range tasks {
-	// 	if task.Id == "6" {
-	// 		task.Title = simplesql.taskDTO //надо создать http запрос
-	// 		//и не получить тело JSON
-	// 		task.
-	// 		if err := simplesql.UpdateTask(ctx, conn, task); err != nil{
-	// 			panic(err)
-	// 		}
-	// 		break
-	// 	}
-	// }
+	for _, task := range tasks {
+		if task.Id == "6" {
+			task.Title = simplesql.taskDTO //надо создать http запрос
+			//и получить тело JSON
+			task.
+			if err := simplesql.UpdateTask(ctx, conn, task); err != nil{
+				panic(err)
+			}
+			break
+		}
+	}
 
 }
